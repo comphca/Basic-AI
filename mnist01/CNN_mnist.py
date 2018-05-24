1097123a5615d8392fc7e9d46d187a426043be17
comphca <comphca@gmail.com>
2018-05-24 13:01:31 +0800

    1

--- mnist01/CNN_mnist.py
+++ mnist01/CNN_mnist.py
@@ -10,15 +10,20 @@ f = np.load(path)
 x_Train, y_Train = f['x_train'], f['y_train']
 x_Test, y_Test = f['x_test'], f['y_test']
 
+#
 x_Train4D = x_Train.reshape(x_Train.shape[0],28,28,1).astype('float32')
 x_Test4D = x_Test.reshape(x_Test.shape[0],28,28,1).astype('float32')
 
+#image图像标准化，除以255
 x_Train4D_normalize = x_Train4D / 255
 x_Test4D_normalize = x_Test4D / 255
 
+#label处理，以One-Hot Encoding编码处理    0000000100则表示数字7
 y_TrainOneHot = np_utils.to_categorical(y_Train)
 y_testOneHot = np_utils.to_categorical(y_Test)
 
+
+#建立一个Sequential模型，后续使用model.add()增加网络层
 model = Sequential()
 
 model.add(Conv2D(filters=16,
